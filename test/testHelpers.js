32183a02e3fcecd7d14f101a47a8d3fa924ed492
malouro <michael.aa.louro@gmail.com>
2019-12-21 22:54:53 -0500

    test: update existing tests & testHelpers
    Add docs for testHelpers
    Update tests for HelpCommand
    Update tests for bot setup & getCommands utility func

--- test/testHelpers.js
+++ test/testHelpers.js
@@ -3,11 +3,27 @@ import { Bot, Command } from '../src/classes'
 import { loggerNames } from '../src/utils/logger'
 import { getCommands } from '../src/utils/setup'
 import { makeCommandFromModule } from '../src/utils/commands'
+import { Collection, Client, GuildMember } from 'discord.js'
 
+/** @description A mock of a Discord OAUTH token */
 export const MockToken = `M${'a'.repeat(23)}.${'a'.repeat(6)}.${'a'.repeat(27)}`
+
+
+/**
+ * @type {string}
+ *
+ * @description
+ * A mock of a Discord user ID to use as the bot's master.
+ */
 export const MockMasterID = 'MasterID'
 
-/* Setup for the mocked Bot */
+
+/**
+ * @typedef {{ commandPrefix: string, statusMessage: string, statusMessageOptions: { type: string, url: string }}}
+ * 
+ * @description
+ * A mock of the config to use for the test bot. Follows the template defined in `/src/constants/config.js`
+ */
 export const MockConfig = {
 	commandPrefix: 'testPrefix',
 	statusMessage: 'testStatusMessage',
@@ -17,14 +33,32 @@ export const MockConfig = {
 	},
 }
 
+
+/**
+ * @type {GuildMember}
+ * 
+ * @description
+ * A basic mock of a Discord.js GuildMember
+ */
 export const MockMember = {
 	hasPermission: () => false,
 	id: 'NotTheMaster :(',
 	guild: { owner: { id: 'NotTheOwner :(' } },
 }
 
+
+/**
+ * @type {EventEmitter}
+ * 
+ * @description
+ * A basic mock of a Discord.js Client
+ */
 export const MockClient = new EventEmitter()
 
+
+/**
+ * @type {{ [key: string]: { log: jest.fn } }}
+ */
 const MockLogger = {}
 
 loggerNames.forEach((type) => {
@@ -32,9 +66,15 @@ loggerNames.forEach((type) => {
 		log: jest.fn(logMessage => logMessage),
 	}
 })
-
 export { MockLogger }
 
+
+/**
+ * @type {Command}
+ * 
+ * @description
+ * A basic mock command to run tests with or on.
+ */
 export class MockCommand extends Command {
 	constructor() {
 		super({
@@ -43,24 +83,76 @@ export class MockCommand extends Command {
 	}
 }
 
-export const MockCommandList = getCommands([makeCommandFromModule(MockCommand)])
 
 /**
- * @property {Discord.Client} client
- * 	Mocked Discord Client instance
- * @property {Object} config
- * 	The MockConfig defined above; serves as the configuration for the MockBot
- * @property {Winston.Logger} logger
- * 	A mocked logger instance that will capture all log events with a Jest mock function
+ * @param {Object} options - List of options to configure the mock command with
+ * 
+ * @description
+ * - This will make a command with the Command class defined in `/src/classes/`.
+ * - The key name of the command class (and prototype property of `name`) will default to 'MockCommand'.
  */
-export const MockBot = new Bot({
+export const makeMockCommand = ({ name = 'MockCommand', ...otherOptions }) => ({
+	[name]: class extends Command {
+		constructor() {
+			super({
+				name,
+				...otherOptions,
+			})
+		}
+	}
+}[name])
+
+
+/**
+ * @type {Collection<String, Command>}
+ *
+ * @description
+ * Uses the `getCommands` utility function to make a `Discord.Collection` containing the `MockCommand` defined above.
+ */
+export const MockCommandList = getCommands([makeCommandFromModule(MockCommand)])
+
+
+/**
+ * @typedef {{ client: Client, config: Object, logger: Logger, commands: Collection<String, Command> }}
+ *
+ * @description
+ * Default options to initialize the test bot with.
+ */
+export const defaultMockBotOptions = {
 	client: MockClient,
 	config: MockConfig,
 	logger: MockLogger,
 	commands: MockCommandList,
+}
+
+
+/**
+ * @type {Bot}
+ * 
+ * A test bot that uses the default options
+ */
+export const MockBot = new Bot({
+	...defaultMockBotOptions
 })
 
-/* Mock options to configure commands with */
+
+/**
+ * - Makes a test bot with the given options.
+ * - Use the config option called `mockCommand` to setup the given command in the test bot's command collection.
+ */
+export const makeMockBot = ({ mockCommand, ...overrides }) => {
+	const commands = mockCommand ? getCommands([makeCommandFromModule(mockCommand)]) : MockCommandList
+
+	return new Bot({
+		commands,
+		...overrides
+	})
+}
+
+/**
+ * @description
+ * Mock options to configure commands with
+ */
 export const MockCommandOptions = {
 	bot: MockBot,
 	client: {},
@@ -84,8 +176,11 @@ export const MockCommandOptions = {
 
 
 /**
- * @func runCommand
- * @param command Direct import of the command being tested
+ * @param {Command} command The command to run. Needs to be instantiated before passed into this function
+ * 
+ * @description
+ * - Executes the `run` method of the given command.
+ * - Returns back the given command options, which should include mock results within `message` if applicable.
  */
 export async function runCommand(command, overrides = {}) {
 	const commandOptions = { ...MockCommandOptions, ...overrides }
--- test/unit-&-integration/commands/debug/Help.test.js
+++ test/unit-&-integration/commands/debug/Help.test.js
@@ -1,18 +1,55 @@
-import { runCommand } from '../../../testHelpers'
+import { runCommand, makeMockBot } from '../../../testHelpers'
 
 import Help from '../../../../src/commands/debug/Help'
 
 describe('Help Command', () => {
 	const HelpCommand = new Help()
+	const bot = makeMockBot({ mockCommand: Help })
 
-	test('returns the main help menu', async () => {
-		const { message: { channel: { send: mockedSend } } } = await runCommand(HelpCommand, { args: ['!help'] })
+	describe('Main help menu', () => {
+		test('returns the main help menu', async () => {
+			const { message: { channel: { send: mockedSend } } } = await runCommand(HelpCommand, { args: ['!help'] })
+			const mainHelpMenu = mockedSend.mock.results[0].value
 
-		expect(mockedSend.mock.results[0].value).toBe('This is the main help menu.')
+			expect(mainHelpMenu).toMatchSnapshot()
+		})
 	})
 
-	test.todo('returns a command\'s help menu')
-	test.todo('returns a category\'s help menu')
-	test.todo('displays chainable and non-chainable arguments in the command help menu')
-	test.todo('displays a command\'s argument usage when available')
+	describe('Help Command help menu', () => {
+		let mockedSend = null
+		let helpCommandHelpMenu = null
+
+		beforeEach(async () => {
+			({ message: { channel: { send: mockedSend } } } = await runCommand(HelpCommand, {
+				args: [
+					'!help', 'help'
+				],
+				bot
+			}))
+
+			helpCommandHelpMenu = mockedSend.mock.results[0].value
+		})
+
+		test('returns the command\'s help menu', async () => {
+			expect(helpCommandHelpMenu).toMatchSnapshot()
+		})
+
+		test.each(
+			HelpCommand.usage.args.map(arg => [arg.name, arg.description])
+		)(
+			'the help command\'s help menu shows the `%s` argument\'s description',
+			async (argName, argDescription = '') => {
+				expect(helpCommandHelpMenu).toEqual(expect.stringContaining(argDescription))
+			}
+		)
+	})
+
+	describe('Command help menu', () => {
+		test.todo('displays chainable and non-chainable arguments in the command help menu')
+		test.todo('displays a command\'s argument usage when available')
+	})
+
+	describe('Command category help menu', () => {
+		test.todo('returns a category\'s help menu')
+	})
 })
