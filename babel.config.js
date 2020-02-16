2e0b8527aa90b498900b011bb24a477233be02a2
Mike Louro <michael.aa.louro@gmail.com>
2020-02-16 14:53:41 -0500

    build: compile cjs and esm packages

--- babel.config.js
+++ babel.config.js
@@ -3,11 +3,28 @@ module.exports = {
 		[
 			'@babel/preset-env',
 			{
+				modules: false,
 				targets: {
-					node: 'current',
+					node: '8',
 				},
 			},
 		],
 	],
 	plugins: ['@babel/plugin-proposal-class-properties'],
+	env: {
+		cjs: {
+			presets: [
+				[
+					'@babel/preset-env',
+					{
+						targets: {
+							node: 8,
+						},
+						useBuiltIns: 'usage',
+					},
+				],
+			],
+			plugins: ['@babel/plugin-transform-classes'],
+		},
+	},
 };
