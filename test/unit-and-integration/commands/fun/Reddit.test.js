5e158b008e48372586ca1059cb0a8f6a4b443450
Mike Louro <michael.aa.louro@gmail.com>
2020-06-29 00:00:28 -0400

    test(Reddit): add test cases
    Also, add helper function for running !reddit in test

--- test/unit-and-integration/commands/fun/Reddit.test.js
+++ test/unit-and-integration/commands/fun/Reddit.test.js
@@ -4,12 +4,17 @@ import { runCommand, MockTranslateFunc } from '../../../testHelpers';
 
 jest.mock('feed-read-parser', () => (url, callback) => {
 	switch (url) {
-		/* Forcing a return of an error */
-		case 'https://www.reddit.com/r/FORCE_ERROR/.rss':
-			return callback(new Error());
+		// Force a return of an error
+		case 'https://www.reddit.com/r/force_error/.rss':
+			return callback('error');
 
-		/* /r/all */
+		// empty subreddit
+		case 'https://www.reddit.com/r/empty/.rss':
+			return callback(null, []);
+
+		// r/all
 		case 'https://www.reddit.com/r/all/.rss':
+			// Slightly modified result from fetching from the r/all .rss
 			return callback(null, [
 				{
 					title: 'POST_TITLE',
@@ -32,7 +37,32 @@ jest.mock('feed-read-parser', () => (url, callback) => {
 				},
 			]);
 
-		// ???
+		case 'https://www.reddit.com/r/image/.rss':
+			return callback(null, [
+				{
+					content: `
+					<img src="https://an.image.src/picture.jpg" />
+					<a href="first:link"></a>
+					<a href="second:link"></a>
+					<a href="https://i.redd.it/this-is-an-image.jpg"></a>
+					`
+				}
+			]);
+
+		case 'https://www.reddit.com/r/no-image/.rss':
+			return callback(null, [
+				{
+					content: `
+						<table>
+							<span>No image</span>
+							<a hef="just"></a>
+							<a hef="some"></a>
+							<a hef="links"></a>
+						</table>
+					`
+				}
+			]);
+
 		default:
 			break;
 	}
@@ -41,17 +71,73 @@ jest.mock('feed-read-parser', () => (url, callback) => {
 describe('Reddit command', () => {
 	const RedditCommand = new Reddit({ t: MockTranslateFunc() });
 
-	test('runs with no args and grabs a post from r/all', async () => {
-		const result = await runCommand(RedditCommand, { args: [] });
+	/**
+	 * @description
+	 * - Runs the Reddit command, with a given set of option overrides
+	 * - Use an array for `args` to list out arguments to be used in the command execution
+	 * @returns Jest Mock results to the mocked out Discord `message.channel.send()`
+	 */
+	const runRedditCommand = async({
+		args = ['!reddit'],
+		...options
+	} = {}) => {
+		const {
+			message: {
+				channel: { send: mockedSend },
+				reply: mockedReply,
+			},
+		} = await runCommand(RedditCommand, {
+			args,
+			...options
+		});
 
-		expect(result.message.channel.send.mock.results[0].value).toMatchSnapshot();
+		return (
+			mockedSend.mock.results.length > 0 ||
+			mockedReply.mock.results.length > 0
+		) ?
+			({
+				sendResult:
+					mockedSend.mock.results[0] &&
+					mockedSend.mock.results[0].value,
+				replyResult:
+					mockedReply.mock.results[0] &&
+					mockedReply.mock.results[0].value,
+			}) :
+			null;
+	};
+
+	test('runs with no args and grabs a post from r/all', async () => {
+		const { sendResult } = await runRedditCommand();
+
+		expect(sendResult).toMatchSnapshot();
+	});
+
+	test('handles posts with and without images', async () => {
+		const { sendResult: resultWithImage} = await runRedditCommand({ args: ['!reddit', 'image']});
+		const { sendResult: resultWithoutImage } = await runRedditCommand({ args: ['!reddit', 'no-image']});
+
+		expect(resultWithImage).toMatchSnapshot();
+		expect(resultWithoutImage).toMatchSnapshot();
 	});
 
 	test('logs any errors from parsing Reddit feed', async () => {
+		const logger = {
+			debug: {
+				log: jest.fn(),
+			},
+		};
 		const result = await runCommand(RedditCommand, {
-			args: ['!reddit', 'FORCE_ERROR'],
+			args: ['!reddit', 'force_error'],
+			logger,
 		});
 
-		expect(result.message.channel.send.mock.results[0].value).toMatchSnapshot();
+		expect(result.message.channel.send).not.toHaveBeenCalled();
+		expect(logger.debug.log).toHaveBeenCalled();
+	});
+
+	test('empty subreddit', async () => {
+		const { replyResult } = await runRedditCommand({ args: ['!reddit', 'empty'] });
+
+		expect(replyResult).toMatchSnapshot();
 	});
 });
